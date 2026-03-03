8b978611c433ebd9169f1113f054b9461dff3ea6
gaurav-kumar26 <gauravmodi1111@gmail.com>
2026-03-03 14:23:27 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/medical/medicalbillportal/MedicalbillportalApplication.java
@@ -0,0 +1,13 @@
+package com.medical.medicalbillportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalbillportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalbillportalApplication.class, args);
+	}
+
+}
