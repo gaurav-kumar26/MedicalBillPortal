bcf94a1dfae7a26c52e25499b70eae0652e2040d
khushigupta10100 <khushigupta10100@gmail.com>
2026-03-06 12:32:08 +0530

    checking push

---
+++ src/test/java/com/medical/medicalbillportal/MedicalbillportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.medicalbillportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalbillportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
